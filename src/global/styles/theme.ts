1aefa3cb520096053ec27f2888f0e4fd12892ad6
Tiago <tiago.gama.ext@dasa.com.br>
2021-07-10 10:25:42 -0300

    feat: add colors in theme file

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+    colors: {
+        primary: '#000000',
+        secondary: '#777777',
+        secondary_light: 'rgba(119, 119, 119,.3)',
+
+        success: '',
+        success_light: '',
+
+        danger: '#E83F5B',
+        danger_light: 'rgba(232, 63, 91, .3)',
+
+        shape: '#FFFFFF',
+        background: '#F0F2F5',
+        title: '#000000',
+        text: '#333333',
+    }
+}
