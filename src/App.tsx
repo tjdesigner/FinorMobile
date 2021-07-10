6354439e30232e3d25f5d0a998eafa0f2e6fcc62
Tiago <tiago.gama.ext@dasa.com.br>
2021-07-10 10:42:45 -0300

    feat: implement themeProvider main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react'
 import { View, Text, SafeAreaView, StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './global/styles/theme'
 
 const App: FC = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <StatusBar backgroundColor='rgba(0,0,0,0.3)' translucent={true} barStyle='default' />
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ color: 'black', fontSize: 40, fontWeight: 'bold' }}> Main </Text>
+    <SafeAreaView>
+      <View>
+        <Text> Main </Text>
       </View>
     </SafeAreaView>
-  </>
+  </ThemeProvider>
 )
 
 export default App
